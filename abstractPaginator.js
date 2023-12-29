3fc6eea2c6ebe094203fc1f74faa4132faf4a109
Amar kumar singh <amar@ostrojs.com>
2023-12-29 15:49:10 +0530

    Update abstractPaginator.js

--- abstractPaginator.js
+++ abstractPaginator.js
@@ -1,313 +1,88 @@
-const querystring = require('querystring');
-const { range, is_null, is_object } = require('@ostro/support/function');
+
+const AbstractPaginator = require('./abstractPaginator');
 const Collection = require('@ostro/support/collection');
-const { Macroable } = require('@ostro/support/macro');
-class AbstractPaginator extends Macroable {
+const { rtrim } = require('@ostro/support/function')
+class Paginator extends AbstractPaginator {
 
-	$items;
+	$hasMore;
 
-	$perPage;
+	constructor($items, $perPage, $currentPage = null, $options = {}, $request = {}) {
+		super();
+		this.$options = $options;
+		this.$request = $request;
+		for (let $key in $options) {
+			this[$key] = $options[$key];
+		}
 
-	$currentPage;
+		this.$perPage = $perPage;
+		this.$currentPage = this.setCurrentPage($currentPage);
+		this.$path = this.$path !== '/' ? rtrim(this.$path, '/') : this.$path;
 
-	$path = '/';
-
-	$query = [];
-
-	$fragment;
-
-	$pageName = 'page';
-
-	$onEachSide = 3;
-
-	$options;
-
-	$currentPathResolver;
-
-	$currentPageResolver;
-
-	$queryStringResolver;
-
-	static $viewFactoryResolver;
-
-	static $defaultView = 'pagination::tailwind';
-
-	static $defaultSimpleView = 'pagination::simple-tailwind';
-
-	isValidPageNumber($page) {
-		return $page >= 1 && Number.isInteger(parseInt($page)) !== false;
+		this.setItems($items);
 	}
 
-	previousPageUrl() {
-		if (this.currentPage() > 1) {
-			return this.url(this.currentPage() - 1);
+	setCurrentPage($currentPage) {
+		$currentPage = $currentPage ? null : this.resolveCurrentPage();
+
+		return this.isValidPageNumber($currentPage) ? parseInt($currentPage) : 1;
+	}
+
+	setItems($items) {
+		this.$items = $items instanceof Collection ? $items : Collection.make($items);
+
+		this.$hasMore = this.$items.count() > this.$perPage;
+
+		this.$items = this.$items.slice(0, this.$perPage);
+	}
+
+	nextPageUrl() {
+		if (this.hasMorePages()) {
+			return this.url(this.currentPage() + 1);
 		}
 	}
 
-	getUrlRange($start, $end) {
-		return new Collection(range($start, $end)).mapWithKeys(($page) => {
-			return [$page, this.url($page)];
-		}).all();
-	}
-
-	url($page) {
-		if ($page <= 0) {
-			$page = 1;
+	nextPage() {
+		if (this.hasMorePages()) {
+			return this.currentPage() + 1;
 		}
-
-		const $parameters = { [this.$pageName]: $page };
-
-		if (count(this.$query) > 0) {
-			Object.assign($parameters, this.$query);
-		}
-
-		return this.path()
-			+ (this.path().includes('?') ? '&' : '?')
-			+ querystring.stringify($parameters)
-			+ this.buildFragment();
 	}
 
-	fragment($fragment = null) {
-		if (is_null($fragment)) {
-			return this.$fragment;
-		}
+	links($view = null, $data = []) {
+		return this.render($view, $data);
+	}
 
-		this.$fragment = $fragment;
+	render($view = null, $data = {}) {
+		return this.viewFactory().make($view ? null : this.$defaultSimpleView, Object.assign($data, {
+			'paginator': this,
+		}));
+	}
+
+	hasMorePagesWhen($hasMore = true) {
+		this.$hasMore = $hasMore;
 
 		return this;
 	}
 
-	appends($key, $value = null) {
-		if (is_null($key)) {
-			return this;
-		}
-
-		if (is_object($key)) {
-			return this.appendObject($key);
-		}
-
-		return this.addQuery($key, $value);
+	hasMorePages() {
+		return this.$hasMore;
 	}
 
-	appendObject($keys = {}) {
-		for (const key in $keys) {
-			this.addQuery(key, $keys[key]);
-		}
-
-		return this;
+	toJson() {
+		return {
+			'current_page': this.currentPage(),
+			'data': this.items(),
+			'first_page_url': this.url(1),
+			'from': this.firstItem(),
+			'next_page_url': this.nextPageUrl(),
+			'path': this.path(),
+			'per_page': this.perPage(),
+			'prev_page_url': this.previousPageUrl(),
+			'to': this.lastItem(),
+		};
 	}
 
-	withQueryString() {
-		return this.appends(this.resolveQueryString());
-	}
-
-	addQuery($key, $value) {
-		if ($key !== this.$pageName) {
-			this.$query[$key] = $value;
-		}
-
-		return this;
-	}
-
-	buildFragment() {
-		return this.$fragment ? '#' + this.$fragment : '';
-	}
-
-	loadMorph($relation, $relations) {
-		this.getCollection().loadMorph($relation, $relations);
-
-		return this;
-	}
-
-	loadMorphCount($relation, $relations) {
-		this.getCollection().loadMorphCount($relation, $relations);
-
-		return this;
-	}
-
-	items() {
-		return this.$items.all();
-	}
-
-	firstItem() {
-		return count(this.$items) > 0 ? (this.$currentPage - 1) * this.$perPage + 1 : null;
-	}
-
-	lastItem() {
-		return count(this.$items) > 0 ? this.firstItem() + this.count() - 1 : null;
-	}
-
-	through($callback) {
-		this.$items.transform($callback);
-
-		return this;
-	}
-
-	perPage() {
-		return this.$perPage;
-	}
-
-	hasPages() {
-		return this.currentPage() != 1 || this.hasMorePages();
-	}
-
-	onFirstPage() {
-		return this.currentPage() <= 1;
-	}
-
-	onLastPage() {
-		return !this.hasMorePages();
-	}
-
-	currentPage() {
-		return this.$currentPage;
-	}
-
-	getPageName() {
-		return this.$pageName;
-	}
-
-	setPageName($name) {
-		this.$pageName = $name;
-
-		return this;
-	}
-
-	withPath($path) {
-		return this.setPath($path);
-	}
-
-	setPath($path) {
-		this.$path = $path;
-
-		return this;
-	}
-
-	onEachSide($count) {
-		this.$onEachSide = $count;
-
-		return this;
-	}
-
-	path() {
-		return this.$path;
-	}
-
-	resolveCurrentPath($default = '/') {
-		return this.$request ? this.$request.url() : $default;
-
-	}
-
-	resolveCurrentPage($pageName = 'page', $default = 1) {
-		const page = this.$request ? this.$request.input($pageName) : null;
-		const parsedPage = parseInt(page, 10);
-		if (!isNaN(parsedPage) && Number.isInteger(parsedPage) && parsedPage >= 1) {
-			return parsedPage;
-		}
-		return $default;
-	}
-
-	resolveQueryString($default = null) {
-		return this.$request ? this.$request.getQuery() : $default;
-	}
-
-	static viewFactory() {
-		return this.$viewFactoryResolver();
-	}
-
-	static viewFactoryResolver($resolver) {
-		this.$viewFactoryResolver = $resolver;
-	}
-
-	static defaultView($view) {
-		this.$defaultView = $view;
-	}
-
-	static defaultSimpleView($view) {
-		this.$defaultSimpleView = $view;
-	}
-
-	static useTailwind() {
-		this.defaultView('pagination::tailwind');
-		this.defaultSimpleView('pagination::simple-tailwind');
-	}
-
-	static useBootstrap() {
-		this.useBootstrapFour();
-	}
-
-
-	static useBootstrapThree() {
-		this.defaultView('pagination::default');
-		this.defaultSimpleView('pagination::simple-default');
-	}
-
-	static useBootstrapFour() {
-		this.defaultView('pagination::bootstrap-4');
-		this.defaultSimpleView('pagination::simple-bootstrap-4');
-	}
-
-	static useBootstrapFive() {
-		this.defaultView('pagination::bootstrap-5');
-		this.defaultSimpleView('pagination::simple-bootstrap-5');
-	}
-
-	getIterator() {
-		return this.$items.getIterator();
-	}
-
-	isEmpty() {
-		return this.$items.isEmpty();
-	}
-
-	isNotEmpty() {
-		return this.$items.isNotEmpty();
-	}
-
-	count() {
-		return this.$items.count();
-	}
-
-	getCollection() {
-		return this.$items;
-	}
-
-	setCollection($collection) {
-		this.$items = $collection;
-
-		return this;
-	}
-
-	getOptions() {
-		return this.$options;
-	}
-
-	offsetExists($key) {
-		return this.$items.has($key);
-	}
-
-	offsetGet($key) {
-		return this.$items.get($key);
-	}
-
-	offsetSet($key, $value) {
-		this.$items.put($key, $value);
-	}
-
-	offsetUnset($key) {
-		this.$items.forget($key);
-	}
-
-	toHtml() {
-		return String(this.render());
-	}
-
-	__call($target, $method, $parameters) {
-		return $target.getCollection()[$method](...$parameters);
-	}
-
-	__toString() {
-		return String(this.render());
+	jsonSerialize() {
+		return this.toJson();
 	}
 }
-module.exports = AbstractPaginator
+module.exports = Paginator
